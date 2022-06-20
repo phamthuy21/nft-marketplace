9f7a60e92c81e85dabceecd99499f2dbcb7ecf36
phamthuy21 <phamthuy2192001@gmail.com>
2022-06-20 19:20:45 +0700

    complete nftmarketplace

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Choose from "./components/Choose";
+import Clients from "./components/Clients";
+import CreateAndSell from "./components/CreateAndSell";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Marketplace from "./components/Marketplace";
+import ScrollToTop from "./components/ScrollToTop";
+import Subscribe from "./components/Subscribe";
+
+export default function App() {
+  return (
+    <div>
+      <ScrollToTop />
+      <Home />
+      <Clients />
+      <CreateAndSell />
+      <Choose />
+      <Marketplace />
+      <Subscribe />
+      <Footer />
+    </div>
+  );
+}
